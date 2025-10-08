6d667dd998319d59249c396e4bc33c0842f7917e
RobertoSan313 <pkgamer313@gmail.com>
2025-10-08 09:04:11 -0500

    semana 9

--- src/Semana09_MatrizNotas.java
+++ src/Semana09_MatrizNotas.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Semana09_MatrizNotas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedir cantidad de estudiantes y materias
+        System.out.print("Ingrese el número de estudiantes: ");
+        int estudiantes = sc.nextInt();
+
+        System.out.print("Ingrese el número de materias: ");
+        int materias = sc.nextInt();
+
+        // Crear matriz de notas
+        double[][] notas = new double[estudiantes][materias];
+
+        // Llenar la matriz con las notas
+        System.out.println("\n--- Ingrese las notas ---");
+        for (int i = 0; i < estudiantes; i++) {
+            System.out.println("Estudiante " + (i + 1) + ":");
+            for (int j = 0; j < materias; j++) {
+                System.out.print("  Nota en materia " + (j + 1) + ": ");
+                notas[i][j] = sc.nextDouble();
+            }
         }
+
+        // Variables para máximo y mínimo
+        double max = notas[0][0];
+        double min = notas[0][0];
+
+        System.out.println("\n--- Promedios por estudiante ---");
+
+        // Calcular promedios y buscar máximo y mínimo
+        for (int i = 0; i < estudiantes; i++) {
+            double suma = 0;
+            for (int j = 0; j < materias; j++) {
+                double nota = notas[i][j];
+                suma += nota;
+
+                // Actualizar máximo y mínimo
+                if (nota > max) max = nota;
+                if (nota < min) min = nota;
+            }
+            double promedio = suma / materias;
+            System.out.printf("Estudiante %d -> Promedio: %.2f%n", (i + 1), promedio);
+        }
+
+        // Mostrar resultados finales
+        System.out.println("\n--- Resultados globales ---");
+        System.out.println("Nota máxima: " + max);
+        System.out.println("Nota mínima: " + min);
+
+        sc.close();
     }
 }
